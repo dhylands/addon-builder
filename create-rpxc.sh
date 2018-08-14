f3a15c0d844b9fcf98bf7525e7fe64150239fdfd
Dave Hylands <dhylands@gmail.com>
2018-08-14 16:12:11 -0700

    Use rpxc updated from jessie to stretch

--- create-rpxc.sh
+++ create-rpxc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 RPXC="./bin/rpxc"
 
@@ -7,14 +8,14 @@ mkdir -p $(dirname ${RPXC})
 
 # Build the docker raspberry pi cross compiler
 echo "Creating rpxc"
-docker run sdthirlwall/raspberry-pi-cross-compiler > ${RPXC}
+docker run dhylands/raspberry-pi-cross-compiler-stretch > ${RPXC}
 chmod +x ${RPXC}
 
-sudo ${RPXC} apt update
-sudo ${RPXC} apt upgrade
+#sudo ${RPXC} apt update
+#sudo ${RPXC} apt upgrade
 
-${RPXC} install-raspbian libudev-dev
-${RPXC} install-debian pkg-config
-${RPXC} install-debian python
-${RPXC} install-debian python2.7
-${RPXC} install-debian unzip
+#${RPXC} install-raspbian libudev-dev
+#${RPXC} install-debian pkg-config
+#${RPXC} install-debian python
+#${RPXC} install-debian python2.7
+#${RPXC} install-debian unzip
